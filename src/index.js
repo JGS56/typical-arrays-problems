61bac193e4d9b3425e3f3a8138564ef7448412c5
Sergey Kostin <61797388+JGS56@users.noreply.github.com>
2021-03-09 17:40:55 +0500

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = "empty") {
+  if (array === "empty" || array.length === 0) {
+      return 0;
+  } else {
+      let min = array[0];
+      for (let i = 0; i < array.length; i++) {
+          if (min > array[i]) {
+              min = array[i];
+          }        
+      }
+      return min;
+  } 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function (array = "empty") {
+  if (array === "empty" || array.length === 0) {
+      return 0;
+  } else {
+      let max = array[0];
+      for (let i = 0; i < array.length; i++) {
+          if (max < array[i]) {
+              max = array[i];
+          }        
+      }
+      return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function(array = "empty") {
+  if (array === "empty" || array.length === 0) {
+      return 0;
+  } else {
+      let sumOfEle = 0;
+      for (let i = 0; i < array.length; i++) {
+          sumOfEle += array[i];        
+      }
+      return sumOfEle / array.length;
+  }
+}
 }
